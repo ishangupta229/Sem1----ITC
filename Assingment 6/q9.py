06fed2d7a9a8ba6546d44c4f03e7f4935694a9d1
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2023-02-23 12:06:25 +0530

    Update q9.py

---
+++ Assingment 6/q9.py
@@ -0,0 +1,11 @@
+
+x= input("Enter the string : ")
+
+def is_pangram(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+        if char not in string.lower():
+            return False
+    return True
+    
+print(is_pangram(x))
